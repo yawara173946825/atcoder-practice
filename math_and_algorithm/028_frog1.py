7674cb35c026317bd521d6bf2cd612b03edf6b1d
yawara173946825 <yawara173946825@gmail.com>
2024-09-11 22:53:17 +0900

    028_frog1の修正

--- math_and_algorithm/028_frog1.py
+++ math_and_algorithm/028_frog1.py
@@ -7,9 +7,10 @@ for i in range(1,N):
     if i == 1:
         dp[i] = abs(H[i-1] - H[i])
     if i >= 2:
-        v1 = dp[i-1] + H[i-1] - H[i]
-        v2 = dp[i-2] + H[i-2] - H[i]
+        v1 = abs(dp[i-1] + H[i-1] - H[i])
+        v2 = abs(dp[i-2] + H[i-2] - H[i])
         dp[i] = min(v1,v2)
 
 print(dp[N-1])
+print(dp)
     
